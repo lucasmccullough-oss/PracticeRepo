59a51793025d726bc1b8c166808f14592482f10d
lucasmccullough-oss <lucasmccullough@arizona.edu>
2026-09-24 10:20:02 -0700

    commiting

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		//print "Hello GitHub"
+		System.out.println("Hello GitHub");
+
+		//print your name
+		System.out.println("Luke");
+		//make any other edits to this file you want, get creative!
+
+		
+	}
+}
